eafff8d9bd46ad6ade67ca3a60f3c464d85ccad3
Michelle Morales <michellemoralesm222@gmail.com>
2025-05-15 11:55:20 -0500

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
 function upDate(previewPic) {
     let image = document.getElementById("image");
     image.style.backgroundImage = "url('" + previewPic.src + "')";
-    image.innerHTML = previewPic.alt; // Actualiza el texto con el 'alt' de la imagen
+    image.innerHTML = previewPic.alt;
   }
   
   function unDo() {
     let image = document.getElementById("image");
-    image.style.backgroundImage = "url('')"; // Elimina la imagen de fondo
-    image.innerHTML = "Hover over an image below to display here."; // Restablece el texto
+    image.style.backgroundImage = "url('')";
+    image.innerHTML = "Hover over an image below to display here.";
   }
   
